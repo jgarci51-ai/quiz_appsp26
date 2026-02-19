4a41ff2d1bf56634531eff6ac0dbee2ae64351cd
jgarci51-ai <jgarci51@nyit.edu>
2026-02-19 11:04:40 -0500

    Made answer button widget and loaded on questions screen

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -17,6 +17,10 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
         backgroundColor: const Color.fromARGB(255, 33, 1, 95),
         foregroundColor: Colors.white,
       ),
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_appsp26/answer_button.dart';
+
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -25,11 +27,11 @@ Widget build(BuildContext context){
             )
           ),
         const SizedBox(height: 30),
-        ElevatedButton(onPressed: (){}, child: const Text('Answer 1')),
-        ElevatedButton(onPressed: (){}, child: const Text('Answer 2')),
-        ElevatedButton(onPressed: (){}, child: const Text('Answer 3')),
-        ElevatedButton(onPressed: (){}, child: const Text('Answer 4'))
+        AnswerButton(answerText: "Answer 1", onTap: (){}),
+        AnswerButton(answerText: "Answer 2", onTap: (){}),
+        AnswerButton(answerText: "Answer 3", onTap: (){}),
+        AnswerButton(answerText: "Answer 4", onTap: (){}),
         ],),
     );
-}
+  }
 }
